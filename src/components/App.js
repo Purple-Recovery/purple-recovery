0ed966d99238d5c528cac9c60528ca69832d5ba2
Kyler Sakumoto <kylerws@uw.edu>
2020-04-28 23:11:19 -0700

    Edited App.js comments

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
 import '../css/App.css';
 
+// Contains the React Router for the website
 class App extends Component {
   render() {
     return (
-      <div>
-          Ain't nothin here yet
-      </div>
+      <div></div>
     );
   } 
 }
