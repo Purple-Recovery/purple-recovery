218cee454a3afd938b0746f15ded7f666a7caf2f
Godgiven Reamico <caelin30@uw.edu>
2020-04-29 03:37:00 -0700

    Updated the comment header for this one as well!

--- src/components/BreadcrumbNav.js
+++ src/components/BreadcrumbNav.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 
-// 
+// This component displays a line of text that will displau the users's directory
+// relative to the main page. This provides a one0click access to each of the parent page
+// This communicates with the previous page.
+ // Resides client-side only
 class BreadcrumbNavView extends Component {
 
 }
