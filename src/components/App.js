173c5b56052ae3338f9c91b8d0cf089be4169543
Kyler Sakumoto <kylerws@uw.edu>
2020-04-28 23:29:21 -0700

    Added stubs for all views

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import '../css/App.css';
 
 // Contains the React Router for the website
+// 
 class App extends Component {
   render() {
     return (
---
+++ src/components/Dashboard.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+
+//
+class DashboardView extends Component {
+
+}
+
+//
+class HeaderView extends Component {
+
+}
+
+//
+class BannerView extends Component {
+
+}
+
+//
+class SectionView extends Component {
+
+}
+
+//
+class TrackersView extends Component {
+
+}
+
+//
+class BreadcrumbNavView extends Component {
+
+}
+
+export {DashboardView, HeaderView, BannerView, SectionView, TrackersView, BreadcrumbNavView};
