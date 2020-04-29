043bb461675ca264738d1e76a313713f6ff60e01
Kyler Sakumoto <kylerws@uw.edu>
2020-04-28 19:49:44 -0700

    Created DropANote.js

---
+++ src/components/DropANote.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class DropANote extends Component {
+  
+  
+
+
+  render() {
+    return (
+      <div>
+          Ain't nothin here yet
+      </div>
+    );
+  } 
+}
+
+export default DropANote;
