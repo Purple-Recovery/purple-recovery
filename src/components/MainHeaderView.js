6e16ad57438f02aa4e5c8a1dbb43595baea44c56
gcaelin30 <caelin30@uw.edu>
2020-06-02 19:16:49 -0700

    hi! Changed the 1.7 requirement! updated it to proper home link

--- src/components/MainHeaderView.js
+++ src/components/MainHeaderView.js
@@ -8,13 +8,15 @@ class MainHeaderView extends Component {
   render() {
     return (
       <div class="h-10 mainHeader">
-        {/* todo: update hrefs to homepage, once hosting etc figured out */}
+        {/* todo: update hrefs to homepage, Completed! https://purple-recovery.web.app/ */}
         <div>
           {/* <h1 class="mainHeaderMain">PURPLE RECOVERY</h1> */}
-          <a href="https://google.com" class="mainHeaderMain">PURPLE RECOVERY</a>
+          <a href="https://purple-recovery.web.app/" class="mainHeaderMain">
+            PURPLE RECOVERY
+          </a>
         </div>
         <div>
-          <a href="https://google.com" class="mainHeaderSubtitle">
+          <a href="https://purple-recovery.web.app/" class="mainHeaderSubtitle">
             COVID-19 INFORMATION FOR UW STUDENTS
           </a>
         </div>
